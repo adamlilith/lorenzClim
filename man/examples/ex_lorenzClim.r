871b9ed27dfa313eb63451f9161d1e9f6ea63f34
Adam B. Smith <adam@earthskysea.org>
2023-06-19 12:18:41 -0500

    Update ex_lorenzClim.r

--- man/examples/ex_lorenzClim.r
+++ man/examples/ex_lorenzClim.r
@@ -9,82 +9,123 @@
 # example is for a Windows system:
 lorenz <- 'C:/Ecology/Data/Lorenz et al 2016'
 
-# one variable and one GCM
-out <- lorenzClim(
+library(terra)
+
+# one variable and one GCM, collated first across years by month
+outMxM <- lorenzClim(
     var = 'tmean',
     summary = '*',
     start = 2005,
     end = 2007,
     rcp = 85,
     gcm = 'ACCESS1-3',
-    lorenz = lorenz
+    lorenz = lorenz,
+    yearByYear = FALSE
 )
 
-plot(out)
+outMxM
+
+# one variable and one GCM, collated first across years by month
+outYxY <- lorenzClim(
+    var = 'tmean',
+    summary = '*',
+    start = 2005,
+    end = 2007,
+    rcp = 85,
+    gcm = 'ACCESS1-3',
+    lorenz = lorenz,
+    yearByYear = TRUE
+)
+
+outYxY
+
+plot(outMxM[['an_sd_TMEAN']] - outYxY[['an_sd_TMEAN']])
 
 # two variables and one GCM
-out <- lorenzClim(
-    var = c('tmean', 'prec'),
+twos <- lorenzClim(
+    var = c('tmin', 'tmax'),
     summary = 'annual',
     start = 2005,
     end = 2007,
     rcp = 85,
     gcm = 'ACCESS1-3',
-    lorenz = lorenz
+    lorenz = lorenz,
+    yearByYear = FALSE
 )
 
-plot(out)
+plot(twos)
+
+# one variable and two RCPs
+rcps <- lorenzClim(
+    var = 'gdd0',
+    summary = 'monthly',
+    start = 2005,
+    end = 2007,
+    rcp = '*',
+    gcm = 'ACCESS1-3',
+    lorenz = lorenz,
+    yearByYear = FALSE
+)
+
+plot(rcps)
 
 # two variables and two GCMs
-out <- lorenzClim(
-    var = c('tmean', 'prec'),
-    summary = 'annual',
+et <- lorenzClim(
+    var = c('PET', 'AET'),
+    summary = 'quarter',
     start = 2005,
     end = 2007,
     rcp = 85,
     gcm = c('ACCESS1-3', 'CanESM2'),
-    lorenz = lorenz
+    lorenz = lorenz,
+    yearByYear = FALSE,
+	verbose = TRUE
 )
 
-out
+et
 
-# one variable, all GCMs, one summary
-out <- lorenzClim(
-    var = 'PET',
+# one variable, all GCMs, one summary statistic
+wind <- lorenzClim(
+    var = 'wind',
     summary = 'summary',
     start = 2005,
     end = 2007,
     rcp = 85,
     gcm = '*',
-    lorenz = lorenz
+    lorenz = lorenz,
+    yearByYear = FALSE,
+	verbose = TRUE
 )
 
-out
+wind
 
 # "raw" monthly rasters
-out <- lorenzClim(
-    var = 'wind',
+raw <- lorenzClim(
+    var = 'etr',
     summary = 'raw',
     start = 2005,
     end = 2007,
     rcp = 85,
     gcm = 'ACCESS1-3',
-    lorenz = lorenz
+    lorenz = lorenz,
+    yearByYear = FALSE
 )
 
-out
+raw
 
-# EVERYTHING!!!
-out <- lorenzClim(
+# EVERYTHING!!! Can take a few minutes, even for a 3-yr time span...
+all <- lorenzClim(
     var = '*',
     summary = '*',
     start = 2005,
     end = 2007,
-    rcp = 85,
+    rcp = '*',
     gcm = '*',
-    lorenz = lorenz
+    lorenz = lorenz,
+    yearByYear = FALSE,
+	verbose = TRUE
 )
 
-out
+all
 
 }
